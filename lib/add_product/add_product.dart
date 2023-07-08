670f802c2bfdf003180ec11d0fb742d1e0808f3a
Sadyrbek <sadyrbek.momunaliev.bj@gmail.com>
2023-07-08 11:33:07 +0200

    second step

--- lib/add_product/add_product.dart
+++ lib/add_product/add_product.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:advert/components/custom_text_field.dart';
 import 'package:advert/models/constants/app_sizes.dart';
+import 'package:advert/services/date_time_service.dart';
 import 'package:flutter/material.dart';
 
 class AddProduct extends StatefulWidget {
@@ -26,20 +29,38 @@ class _AddProductState extends State<AddProduct> {
         padding: const EdgeInsets.all(10.0),
         child: ListView(
           children: [
-            CustomTextField(hintText: 'title', 
-            controller: _title,
+            CustomTextField(
+              hintText: 'title',
+              controller: _title,
             ),
             AppSizes.height10,
-            CustomTextField(hintText: 'description', controller: _description,
-            maxLines: 5,),
+            CustomTextField(
+              hintText: 'description',
+              controller: _description,
+              maxLines: 5,
+            ),
             AppSizes.height10,
-            CustomTextField(hintText: 'name',controller: _name,),
+            CustomTextField(
+              hintText: 'name',
+              controller: _name,
+            ),
             AppSizes.height10,
-            CustomTextField(hintText: 'datatime',controller: _datatime,),
+            CustomTextField(
+              hintText: 'datatime',
+              controller: _datatime,
+              onTap: () async {
+                await DateTimeService.showDateTimePicker(context, (value) {
+                  _datatime.text = value.toString();
+                });
+              },
+            ),
             AppSizes.height10,
-            CustomTextField(hintText: 'phonenumber',controller: _phonenumber,),
+            CustomTextField(
+              hintText: 'phonenumber',
+              controller: _phonenumber,
+            ),
             AppSizes.height10,
-            CustomTextField(hintText: 'address',controller: _address),
+            CustomTextField(hintText: 'address', controller: _address),
           ],
         ),
       ),
@@ -47,25 +68,3 @@ class _AddProductState extends State<AddProduct> {
   }
 }
 
-class CustomTextField extends StatelessWidget {
-  const CustomTextField({
-    super.key, required this.hintText, this.controller, this.maxLines,
-  });
-  final String hintText;
-  final TextEditingController? controller;
-  final int? maxLines;
-
-  @override
-  Widget build(BuildContext context) {
-    return TextFormField( 
-      controller: controller,
-      maxLines: maxLines,
-      decoration: InputDecoration(
-        border: const OutlineInputBorder(),
-        filled: true,
-        hintText: hintText,
-      ),
-      
-    );
-  }
-}
